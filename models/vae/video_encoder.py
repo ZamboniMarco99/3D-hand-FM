b3ae3a1737043acef306c12a2aca688f96653273
marco <marcozamboni2013107@gmail.com>
2024-09-30 07:08:08 +0200

    Custom initialization of mvit model

--- models/vae/video_encoder.py
+++ models/vae/video_encoder.py
@@ -12,7 +12,159 @@ Example usage:
 
 import torch
 from torch import nn
-from torchvision.models.video import MViT_V2_S_Weights, mvit_v2_s
+from torchvision.models.video.mvit import MSBlockConfig, MViT, _mvit
+
+
+def get_mvit_v2_s_block_setting() -> list[MSBlockConfig]:
+    """Get the block setting for MViT v2 Small architecture.
+
+    Returns:
+        list[MSBlockConfig]: A list of MSBlockConfig objects, each representing a block's configuration.
+
+    """
+    config: dict[str, list] = {
+        "num_heads": [1, 2, 2, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 8, 8],
+        "input_channels": [96, 96, 192, 192, 384, 384, 384, 384, 384, 384, 384, 384, 384, 384, 384, 768],
+        "output_channels": [96, 192, 192, 384, 384, 384, 384, 384, 384, 384, 384, 384, 384, 384, 768, 768],
+        "kernel_q": [
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+        ],
+        "kernel_kv": [
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+            [3, 3, 3],
+        ],
+        "stride_q": [
+            [1, 1, 1],
+            [1, 2, 2],
+            [1, 1, 1],
+            [1, 2, 2],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 1, 1],
+            [1, 2, 2],
+            [1, 1, 1],
+        ],
+        "stride_kv": [
+            [1, 8, 8],
+            [1, 4, 4],
+            [1, 4, 4],
+            [1, 2, 2],
+            [1, 2, 2],
+            [1, 2, 2],
+            [1, 2, 2],
+            [1, 2, 2],
+            [1, 2, 2],
+            [1, 2, 2],
+            [1, 2, 2],
+            [1, 2, 2],
+            [1, 2, 2],
+            [1, 2, 2],
+            [1, 1, 1],
+            [1, 1, 1],
+        ],
+    }
+
+    return [
+        MSBlockConfig(
+            num_heads=config["num_heads"][i],
+            input_channels=config["input_channels"][i],
+            output_channels=config["output_channels"][i],
+            kernel_q=config["kernel_q"][i],
+            kernel_kv=config["kernel_kv"][i],
+            stride_q=config["stride_q"][i],
+            stride_kv=config["stride_kv"][i],
+        )
+        for i in range(len(config["num_heads"]))
+    ]
+
+
+def mvit_v2_s(
+    *,
+    weights: None = None,
+    progress: bool = True,
+    spatial_size: tuple[int, int] = (224, 224),
+    temporal_size: int = 16,
+    **kwargs: dict,
+) -> MViT:
+    """Constructs a small MViTV2 architecture.
+
+    Architecture based on `Multiscale Vision Transformers <https://arxiv.org/abs/2104.11227>`__ and
+    `MViTv2: Improved Multiscale Vision Transformers for Classification
+    and Detection <https://arxiv.org/abs/2112.01526>`__.
+
+    .. betastatus:: video module
+
+    Args:
+        weights (:class:`~torchvision.models.video.MViT_V2_S_Weights`, optional): The
+            pretrained weights to use. See
+            :class:`~torchvision.models.video.MViT_V2_S_Weights` below for
+            more details, and possible values. By default, no pre-trained
+            weights are used.
+        progress (bool, optional): If True, displays a progress bar of the
+            download to stderr. Default is True.
+        spatial_size (tuple, optional): A tuple of two integers representing the
+            height and width of the input frames. Default is (224, 224).
+        temporal_size (int, optional): An integer representing the number of frames
+            in the input video sequence. Default is 16.
+        **kwargs: Additional parameters passed to the ``torchvision.models.video.MViT``
+            base class. Please refer to the `source code
+            <https://github.com/pytorch/vision/blob/main/torchvision/models/video/mvit.py>`_
+            for more details about this class.
+
+    .. autoclass:: torchvision.models.video.MViT_V2_S_Weights
+            :members:
+
+    """
+    block_setting = get_mvit_v2_s_block_setting()
+    return _mvit(
+        spatial_size=spatial_size,
+        temporal_size=temporal_size,
+        block_setting=block_setting,
+        residual_pool=True,
+        residual_with_cls_embed=False,
+        rel_pos_embed=True,
+        proj_after_attn=True,
+        stochastic_depth_prob=kwargs.pop("stochastic_depth_prob", 0.2),
+        weights=weights,
+        progress=progress,
+        **kwargs,
+    )
 
 
 class VideoEncoder(nn.Module):
@@ -49,12 +201,11 @@ class VideoEncoder(nn.Module):
 
         # Load pre-trained MViT v2 Small model
         self.backbone = mvit_v2_s(
-            weights=MViT_V2_S_Weights.DEFAULT,
             spatial_size=(height, width),
             temporal_size=num_frames,
         )
 
-        backbone_out_features = self.backbone.head.in_features
+        backbone_out_features = get_mvit_v2_s_block_setting()[-1].output_channels
 
         # Remove the classification head
         self.backbone.head = nn.Identity()
